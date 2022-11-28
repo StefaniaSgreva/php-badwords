1c5c8ac6c778c52f0d025cb06bf01d61d60bf78f
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-28 11:27:21 +0100

    some php upgrades

--- badwords.php
+++ badwords.php
@@ -1,40 +1,59 @@
-<?php 
-if(isset($_GET['text']) && $_GET['text'] != ''){
-    $text = $_GET['text'];
-} else {
-    $text = 'empty';
-};
-
-if(isset($_GET['badword']) && $_GET['badword'] != ''){
-    $badword = $_GET['badword'];
-} else {
-    $badword = 'empty';
-};
-
+<?php
 // $text = $_GET['text'];
 // $badword = $_GET['badword'];
 
-$textCensored = str_replace($badword, '***', $text);
+$error = null;
+// $text = $GET['text'] ?? 'empty'; //equivalente di isset
+// $text = $GET['badword'] ?? 'empty';
+
+if ((isset($_GET['text']) && !empty($_GET['text'])) && (isset($_GET['badword']) && !empty($_GET['badword']))) {
+    $text = trim($_GET['text']);
+    $badword = trim($_GET['badword']);
+    $textCensored = str_replace($badword, '***', $text);
+
+} else {
+    $error = 'Error: empty input';
+}
+;
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <!-- styles  -->
-     <link rel="stylesheet" href="main.css">
+    <!-- styles  -->
+    <link rel="stylesheet" href="main.css">
     <title>Bad Words</title>
 </head>
-<body>
-    <div class="response">
-        <p><?php echo "Text: $text" ?></p>
-        <div>Pharagraph lenght: <?php echo strlen($text) ?> </div>
 
-        <p><?php echo "Text: $textCensored" ?></p>
-        <div>Pharagraph lenght: <?php echo strlen($textCensored) ?> </div>
-    </div>  
+<body>
+    <?php if ($error) {
+
+        echo "<h1>$error<h1/>";
+
+    } else { ?>
+    <div class="response">
+        <p>
+            <?php echo "Text: $text" ?>
+        </p>
+        <div>Pharagraph lenght:
+            <?php echo strlen($text) ?>
+        </div>
+
+        <p>
+            <?php echo "Text: $textCensored" ?>
+        </p>
+        <div>Pharagraph lenght:
+            <?php echo strlen($textCensored) ?>
+        </div>
+    </div>
+    <?php } ?>
+
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+<?php
+/*
+""	    "apple"	 NULL	FALSE	0	    undefined	     TRUE	array()	 123
+------------------------------------------------------------------------------
+isset	    TRUE	TRUE	 FALSE	TRUE	TRUE	FALSE	        TRUE	TRUE	TRUE
+empty	    TRUE	FALSE	 TRUE	TRUE	TRUE	TRUE	        FALSE	TRUE	FALSE
+is_null	FALSE	FALSE	 TRUE	FALSE	FALSE	Warning/ TRUE	FALSE	FALSE	FALSE
+*/
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,20 +18,23 @@
     <link rel="stylesheet" href="main.css">
     <title>Bad Words</title>
 </head>
+
 <body>
     <section id="badwords">
         <h1>Detecting Unwanted Words</h1>
         <form action="badwords.php" method="GET">
-            
-            <textarea type="text" name="text" class="textbox"></textarea>
-            <label class="form-label">
-                Text area
-            </label>
 
-            <input type="text" name="badword" placeholder="bad word">
-            
+            <!-- <label for="text" class="form-label">Text</label> -->
+            <textarea type="text" name="text" class="textbox" autocomplete="off" required></textarea>
+
+            <!-- <label for="badword">bad word</label> -->
+            <input type="text" name="badword" placeholder="bad word" required>
+
             <button type="submit">Submit</button>
+            <button type="reset">Cancel</button>
+
         </form>
     </section>
 </body>
+
 </html>
